a01c5b21945b73b91145b0fdfb1c324a6a2faee8
bglb@qq.com <bglb@qq.com>
2020-03-04 23:47:38 +0800

    作业第二次完善

--- BGLB.ASP.NETStudy_01/Default.aspx
+++ BGLB.ASP.NETStudy_01/Default.aspx
@@ -10,10 +10,27 @@
 <body>
     <form id="form1" runat="server">
         <div>
-            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Button" />
-            <asp:GridView ID="GridView1" runat="server" OnSelectedIndexChanged="GridView1_SelectedIndexChanged">
-            </asp:GridView>
+            <asp:DropDownList ID="DropDownList1" runat="server" Height="24px" Width="52px">
+                <asp:ListItem Value="sAll">全部</asp:ListItem>
+                <asp:ListItem Value="sId">学号</asp:ListItem>
+                <asp:ListItem Value="sName">姓名</asp:ListItem>
+                <asp:ListItem Value="sGender">班级</asp:ListItem>
+                <asp:ListItem Value="sAge">年龄</asp:ListItem>
+                <asp:ListItem Value="sRemark">备注</asp:ListItem>
+            </asp:DropDownList><asp:TextBox ID="TextBox1" runat="server" Height="16px" Width="168px"></asp:TextBox>
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="确定" Width="63px" />
         </div>
+            <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" Height="120px" OnLoad="Button1_Click" Width="288px">
+                <Columns>
+                    <asp:BoundField HeaderText="学号" DataField="sId" >
+                    <HeaderStyle Height="20px" />
+                    </asp:BoundField>
+                    <asp:BoundField HeaderText="姓名" DataField="sName" />
+                    <asp:BoundField HeaderText="年龄" DataField="sAge" />
+                    <asp:BoundField HeaderText="班级" DataField="sGender" />
+                    <asp:BoundField HeaderText="备注" DataField="sRemark" />
+                </Columns>
+            </asp:GridView>
     </form>
 </body>
 </html>
--- BGLB.ASP.NETStudy_01/Default.aspx.cs
+++ BGLB.ASP.NETStudy_01/Default.aspx.cs
@@ -10,30 +10,31 @@ namespace BGLB.ASP.NETStudy_01
         {
 
         }
-
-        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
-
         protected void Button1_Click(object sender, EventArgs e)
         {
-            SqlConnection scon = new SqlConnection(" Server=.;Uid=sa;Pwd=991220;Database=Person");//第二步: 建立连接
-            scon.Open();
-            //第三步:打开数据
-            SqlDataAdapter sda = new SqlDataAdapter(" SELECT * FROM Student", scon);
-            //第四步:建立适配器
-            DataSet ds = new DataSet();
-            //第五步:内存申请空间
-            sda.Fill(ds);
-            //第六步:填充数据
-            GridView1.DataSource = ds;
-            //第七步:设定数据源
-            GridView1.DataBind();
-            //第八步:绑定数据
-            scon.Close();
-            //第九步:关闭数据
+            string tj = DropDownList1.Text.Trim();      //获取下拉列表框中的文本，并去掉文本的前后空格.
+            string zhi = TextBox1.Text.Trim();
+            string sql_1 = "SELECT * FROM Student";        
+            string sql_2 = "SELECT * FROM Student WHERE " + tj + "='" + zhi + "'";//根据条件查询
 
+            SqlConnection scon = null;      //先定义
+            SqlDataAdapter sda = null;
+            DataSet ds = null;
+            scon = new SqlConnection("Server=.;Uid=sa;Pwd=991220;Database=Person"); //后使用
+            scon.Open();
+            if (tj == "sAll")
+            {
+                sda = new SqlDataAdapter(sql_1, scon);
+            }
+            else
+            {
+                sda = new SqlDataAdapter(sql_2, scon);
+            }
+            ds = new DataSet();
+            sda.Fill(ds);
+            GridView1.DataSource = ds;
+            GridView1.DataBind();
+            scon.Close();
         }
     }
 }
